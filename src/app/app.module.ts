233127a59cf3bd83416b6711c54d188805b92fc1
matteobavecchi <matteo.bavecchi@stud.unifi.it>
2021-07-20 15:27:42 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WidgetComponent } from './widget/widget.component';
 
 @NgModule({
   declarations: [
@@ -13,4 +15,10 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const element = createCustomElement(WidgetComponent, { injector: this.injector });
+    customElements.define('app-widget', element);
+  }
+  ngDoBootstrap() { }
+}
